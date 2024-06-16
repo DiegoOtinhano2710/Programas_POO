67296b37ddb2d9cb6e875ba235cad70ab9f2ac69
DiegoOtinhano2710 <166763225+DiegoOtinhano2710@users.noreply.github.com>
2024-06-15 21:34:21 -0300

    Add files via upload

--- JUEGO/juego.py
+++ JUEGO/juego.py
@@ -15,6 +15,13 @@ class aplicacion(tk.Tk):
     __boton_iluminado:list
     __lista_botones:list            #son los botones que se van a iluminar
     __lista_botones_usuario:list    #son los botones que va a ir pulsando el usuario
+    #ventana game over
+    __game_over:object
+    __texto_game_over:object
+    __texto_con_el_puntaje:object
+    __puntaje_final:object
+    __boton_reintenar:object
+    __boton_salir:object
     #texto de arriba de la ventana que muestra usuario y puntaje
     __marcador:object
     __label_texto_puntaje:object
@@ -28,8 +35,6 @@ class aplicacion(tk.Tk):
     __boton_confirmar:object
     __Nombre_de_usuario:str
     __datos:list
-    
-
     def __init__(self):
         #esto es la ventana
         super().__init__()
@@ -38,9 +43,9 @@ class aplicacion(tk.Tk):
         self.title("PySimon - Game")
         #esto es para el marcador de puntos
         self.__marcador = tk.LabelFrame(self, width='600', height='20')
-        self.__marcador.grid(row=0, column=0, columnspan=2, sticky='we', ipady=50, ipadx=50, pady=10, padx=10)
+        self.__marcador.grid(row=0, column=0, columnspan=2, sticky='nswe', ipady=50, ipadx=50, pady=10, padx=10)
         fuente = ('Arial', 20)    
-        #esto es para poner el texto "Puntaje"
+        #esto es para poner el nombre del usuario 
         self.__texto_reemplazable=tk.StringVar()
         self.__texto_reemplazable.set('')
         self.__label_texto_puntaje = tk.Label(self.__marcador, textvariable=self.__texto_reemplazable, font=fuente)
@@ -55,7 +60,6 @@ class aplicacion(tk.Tk):
         self.__boton_amarillo = tk.Canvas(self, width=230, height=300, bg="#4d4d00")
         self.__boton_azul = tk.Canvas(self, width=230, height=300, bg="#00004d")
         self.__boton_iluminado=None
-
         #aquí se van a guardar los botones que se iluminan para reproducirlos del primero al nuevo que se agrega
         #se establece una lista que va a guardar los botones que clickee el usuario
         self.__lista_botones=[]
@@ -74,6 +78,20 @@ class aplicacion(tk.Tk):
         self.grid_rowconfigure(2, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
+        #Para ventana game over
+        self.__game_over=tk.LabelFrame(self, width=300, height=250)
+        self.__game_over.place(relx=0.5, rely=0.5, anchor='center')
+        self.__texto_game_over=tk.Label(self.__game_over, text='GAME OVER', font=('Arial', 20))
+        self.__texto_game_over.grid(row=0, column=0, columnspan=3,sticky='nswe', ipady=10, ipadx=10)
+        self.__texto_con_el_puntaje=tk.StringVar()
+        self.__texto_con_el_puntaje.set('')
+        self.__puntaje_final=tk.Label(self.__game_over, textvariable=self.__texto_con_el_puntaje, font=('Arial', 14))
+        self.__puntaje_final.grid(row=1, column=0, columnspan=3, sticky='nswe', ipady=10, ipadx=10)
+        self.__boton_salir=tk.Button(self.__game_over, text='Salir', font=('Arial', 12), command=self.destroy)
+        self.__boton_salir.grid(row=3,column=2,sticky='nswe', ipady=10, ipadx=10)
+        self.__boton_reintenar=tk.Button(self.__game_over, text='Volver a intentar', font=('Arial',12), command=self.reintentar)
+        self.__boton_reintenar.grid(row=3,column=0,sticky='nswe', ipady=10, ipadx=10)
+        self.__game_over.place_forget()
         #item2
         fuente2=('Arial', 9)
         self.__Ventana_Nombre_de_usuario=tk.LabelFrame(self, text='Datos del Jugador', font=fuente2, width= 220, height=200)
@@ -86,6 +104,7 @@ class aplicacion(tk.Tk):
         self.__boton_confirmar.grid(row=2, column=2, sticky='nswe', padx=5, pady=5)
         self.__Ventana_Nombre_de_usuario.wait_window()
 
+        
     def comienzo(self):
         self.__lista_botones.append(random.randint(0, 3))       #genera un número random de entre 4 y, dependiendo de cuál se generó, se prende un boton u otro
         self.__lista_botones_usuario.clear()                    #limpia la lista del anterior juego
@@ -112,8 +131,8 @@ class aplicacion(tk.Tk):
                         self.after(1000, self.comienzo)
                 else:
                     self.guardar_json()
-                    messagebox.showinfo("GAME OVER", f"Puntaje obtenido: {self.__contador_puntos.get()}")
-                    self.destroy()
+                    self.llamar_gameover()
+                    
 
     def detectar_clic(self, event):
         boton_clicado = event.widget
@@ -123,6 +142,20 @@ class aplicacion(tk.Tk):
         col_original=boton.cget("bg")
         boton.configure(bg=self.__colores[num])
         boton.after(300, lambda: boton.configure(bg=col_original))
+
+    def llamar_gameover(self):
+        for boton in self.__botones:            #cuando muestra la pantalla de game over, desvincula el evento de click de los canvas.
+            boton.unbind("<Button-1>")          #esto lo hace para que, si está esa pantalla, no puedas darle click a los botones de fondo. sino, en el json detecta que sigue perdiendo y sigue escribiendo el mismo dato
+        self.__texto_con_el_puntaje.set(f"Puntuación: {self.__contador_puntos.get()}")
+        self.__game_over.place(relx=0.5, rely=0.5, anchor='center')
+    
+    def reintentar(self):
+        for boton in self.__botones:
+            boton.bind("<Button-1>", self.detectar_clic)
+        self.__lista_botones.clear()
+        self.__contador_puntos.set(0)
+        self.__game_over.place_forget()
+        self.ejecutar()
 
     def guardar_Usuario(self):
         self.__Nombre_de_usuario=self.__entry_nombre.get()
@@ -147,6 +180,8 @@ class aplicacion(tk.Tk):
     def to_dict(self):                            #diccionario con los datos del jugador
         fechayhora=datetime.now()
         fechayhora=fechayhora.strftime("%d/%m/%Y %H:%M:%S")
+        if self.__Nombre_de_usuario=='':
+            self.__Nombre_de_usuario='Jugador'
         return{
             'Usuario': self.__Nombre_de_usuario,
             'Fecha y hora': fechayhora,
